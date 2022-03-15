d36fb16b7903df14e8c40aa53c404a9a5a438b61
Anthony <tonymich2012@gmail.com>
2022-03-14 18:01:35 -0700

    adds basic pets index page

---
+++ src/views/PetsIndex.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      pets: [],
+    };
+  },
+  created: function () {
+    this.indexPets();
+  },
+  methods: {
+    indexPets: function () {
+      axios.get("/pets").then((response) => {
+        console.log("pets index", response);
+        this.pets = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="pets-index">
+    <h1>All Pets</h1>
+    <div v-for="pet in pets" v-bind:key="pet.id">
+      <h2>{{ pet.name }}</h2>
+      <img v-bind:src="pet.img_url" v-bind:alt="pet.name" />
+      <p>{{ pet.description }}</p>
+    </div>
+  </div>
+</template>
