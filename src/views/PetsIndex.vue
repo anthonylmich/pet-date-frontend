a3df9c397f3602d8a73894560baca381665803e2
Anthony <tonymich2012@gmail.com>
2022-03-23 12:39:39 -0700

    implements theme on the pet index page.

--- src/views/PetsIndex.vue
+++ src/views/PetsIndex.vue
@@ -22,16 +22,46 @@ export default {
 </script>
 
 <template>
-  <div class="pets-index">
-    <h1>All Pets</h1>
-    <div v-for="pet in pets" v-bind:key="pet.id">
-      <h2>{{ pet.name }}</h2>
-      <router-link v-bind:to="`/pets/${pet.id}`">
-        <img v-bind:src="pet.img_url" v-bind:alt="pet.name" />
-      </router-link>
-      <p>{{ pet.description }}</p>
-      <p>Owner: {{ pet.user.name }}</p>
-      <router-link v-bind:to="`/pets/${pet.id}`">More details</router-link>
-    </div>
-  </div>
+  <body>
+    <main id="main">
+      <!-- ======= Blog Section ======= -->
+      <section id="blog" class="blog">
+        <div class="container" data-aos="fade-up">
+          <div class="row">
+            <div class="col-lg-8 entries">
+              <h1>All Pets</h1>
+              <article class="entry" v-for="pet in pets" v-bind:key="pet.id">
+                <div class="entry-img">
+                  <router-link v-bind:to="`/pets/${pet.id}`">
+                    <img v-bind:src="pet.img_url" v-bind:alt="pet.name" />
+                  </router-link>
+                </div>
+
+                <h2 class="entry-title">
+                  <a href="blog-single.html">
+                    {{ pet.name }}
+                  </a>
+                </h2>
+
+                <div class="entry-content">
+                  <p>
+                    {{ pet.description }}
+                  </p>
+                  <p>Owner: {{ pet.user.name }}</p>
+                  <div class="read-more">
+                    <router-link v-bind:to="`/pets/${pet.id}`">More details</router-link>
+                  </div>
+                </div>
+              </article>
+              <!-- End blog entry -->
+            </div>
+            <!-- End blog entries list -->
+          </div>
+        </div>
+      </section>
+      <!-- End Blog Section -->
+    </main>
+    <!-- End #main -->
+    <div id="app"></div>
+  </body>
 </template>
