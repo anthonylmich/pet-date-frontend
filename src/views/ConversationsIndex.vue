107e7cb125732eae193524df25e09012b490cd30
Anthony <tonymich2012@gmail.com>
2022-03-23 23:20:45 -0700

    adds styling for conversation index

--- src/views/ConversationsIndex.vue
+++ src/views/ConversationsIndex.vue
@@ -22,17 +22,34 @@ export default {
 </script>
 
 <template>
-  <div class="conversations-index">
-    <h1>All Conversations</h1>
-    <div v-for="conversation in conversations" v-bind:key="conversation.id">
-      <!-- <h2>{{ conversation.partner }}</h2> -->
-      <router-link v-bind:to="`/conversations/${conversation.id}`">
-        <h1>Conversation with {{ conversation.partner.name }}</h1>
-      </router-link>
-      <p v-if="conversation.last_message">
-        {{ conversation.last_message.content }}
-      </p>
-      <p v-else>No Messages In Conversation</p>
-    </div>
-  </div>
+  <main id="main">
+    <!-- ======= Blog Single Section ======= -->
+    <section id="blog" class="blog">
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-lg-8 entries">
+            <article class="entry entry-single">
+              <div class="entry-content">
+                <h1>All Conversations</h1>
+                <blockquote v-for="conversation in conversations" v-bind:key="conversation.id">
+                  <router-link v-bind:to="`/conversations/${conversation.id}`">
+                    <div class="blog-author d-flex align-items-center">
+                      <div>
+                        <h4>{{ conversation.partner.name }}</h4>
+                        <p v-if="conversation.last_message">
+                          {{ conversation.last_message.content }}
+                        </p>
+                        <p v-else>No Messages In Conversation</p>
+                      </div>
+                    </div>
+                  </router-link>
+                </blockquote>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Blog Single Section -->
+  </main>
 </template>
